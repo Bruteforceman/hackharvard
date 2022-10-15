d7a0fade723efe63ddfc2a118d5940bf6d78e765
Tasmeem Reza <tasmeemreza@gmail.com>
2022-10-15 04:07:57 -0400

    added db integration

---
+++ server/auth.js
@@ -0,0 +1,3 @@
+const User = require('./user')
+const bcrypt = require('bcrypt')
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const User = require("./user")
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8888
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME;
+const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.cvhon.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+mongoose.connect(mongoURI).then(() => {
+  console.log('Connected to MongoDB')
+}).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('HackHarvard 2022')
@@ -12,5 +24,19 @@ app.listen(port, () => {
   console.log(`Website running at port ${port}`)
 })
 
+app.use(express.json())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  }
+))
 
-
+async function main() {
+  const newUser = new User({
+    name: "tasmeemreza@gmail.com",
+    password: "hahah"
+  })
+  console.log(await newUser.save())
+}
+// main()
